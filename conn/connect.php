d895b2a056421dc7cfa7954fff771a1a6576b814
Wellington Vieira dos Santos <wellington.vsantos@gmail.com>
2023-01-18 17:20:15 -0300

    conexao e index

--- conn/connect.php
+++ conn/connect.php
@@ -1,1 +1,19 @@
-<?php ?>
+<?php
+
+$host = "localhost";
+$database = "ti93phpdb01";
+$user= "root";
+$pass = "";
+$charset = "utf8";
+$port = "3306";
+
+try {
+    $conn = new mysqli($host, $user,$pass,$database,$port);
+    mysqli_set_charset($conn, $charset);
+} catch (\Throwable $th) {
+     echo "Atenção ERRO: ".$th; 
+}
+
+
+// http://127.0.0.1/tiphpnt/conn/connect.php
+?>
